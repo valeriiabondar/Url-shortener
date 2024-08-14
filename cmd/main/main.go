e3ac9b018c0f0238f848f2f2391137a5226a2f4d
Valeriia Bondar <valeryybondar@gmail.com>
2024-08-14 17:36:27 +0300

    add configuration

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,4 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"urlShortener/internal/config"
+)
+
 func main() {
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
 }
---
+++ internal/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http-server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"http-server" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle-timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH environment variable is not set")
+	}
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+	var config Config
+
+	if err := cleanenv.ReadConfig(configPath, &config); err != nil {
+		log.Fatal("cannot read config file: ", err)
+	}
+	return &config
+}
