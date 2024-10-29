abfd6630b67e50eb1673213867a67d2782c25f0d
Valeriia Bondar <valeryybondar@gmail.com>
2024-10-29 12:43:49 +0100

    add authorization

--- internal/config/config.go
+++ internal/config/config.go
@@ -14,9 +14,11 @@ type Config struct {
 }
 
 type HTTPServer struct {
-	Address     string        `yaml:"http-server" env-default:"localhost:8080"`
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle-timeout" env-default:"60s"`
+	User        string        `yaml:"user" env-required:"true"`
+	Password    string        `yaml:"password"  env-required:"true" env:"HTTP_SERVER_PASSWORD"`
 }
 
 func MustLoad() *Config {
